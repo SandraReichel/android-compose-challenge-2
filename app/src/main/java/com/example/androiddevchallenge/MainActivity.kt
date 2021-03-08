68fa9ce44f778a84b4103b6fd33fbba52d066f1e
SandraReichel <sandra.reichel.dresden@gmail.com>
2021-03-09 00:18:39 +0100

    add functioning animations

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,6 +18,11 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,10 +32,15 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
@@ -41,6 +51,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var timerViewModel: TimerViewModel
 
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -55,8 +66,11 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Start building your app here!
+@ExperimentalAnimationApi
 @Composable
 fun MyApp(viewModel: TimerViewModel) {
+
+    //val backgroundColor by animateColorAsState(if (tabPage == TabPage.Home) Purple100 else Green300)
     Surface(color = MaterialTheme.colors.background) {
         Column(
             horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxWidth()
@@ -70,26 +84,45 @@ fun MyApp(viewModel: TimerViewModel) {
                 }
             )
             TimerScreen(viewModel = viewModel)
+            SandClock()
         }
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun TimerScreen(viewModel: TimerViewModel) {
-    val timeLeftInMillis by viewModel.timeLeftInMills.observeAsState(
-
-            viewModel.startTime
-
-        )
-
+    val timeLeftInMillis by viewModel.timeLeftInMills.observeAsState(viewModel.startTime)
     val running by viewModel.isTimerRunning.observeAsState(true)
 
-    TimerControlView(
-        timeLeftInMillis = timeLeftInMillis,
-        changeRunningState = { viewModel.switchTimerRunning() },
-        changeStartTime = { viewModel.startTime = it },
-        running
-    )
+    var panelVisible by rememberSaveable { mutableStateOf(false) }
+
+    AnimatedVisibility(
+        visible = panelVisible,
+        enter = slideInVertically(
+            // Enters by sliding down from offset -fullHeight to 0.
+            initialOffsetY = { fullHeight -> -fullHeight },
+            animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+        ),
+        exit = slideOutVertically(
+            // Exits by sliding up from offset 0 to -fullHeight.
+            targetOffsetY = { fullHeight -> -fullHeight },
+            animationSpec = tween(durationMillis = 250, easing = FastOutLinearInEasing)
+        )
+    ) {
+        TimerControlView(
+            timeLeftInMillis = timeLeftInMillis,
+            changeRunningState = { viewModel.switchTimerRunning() },
+            changeStartTime = { viewModel.startTime = it },
+            running
+        )
+    }
+    Button(onClick = { panelVisible = !panelVisible }) {
+        Row {
+            Icon(Icons.Filled.Settings, contentDescription = null)
+            Text("Setup the Timer")
+        }
+    }
 }
 
 @Composable
@@ -140,6 +173,8 @@ fun TimerControlView(
 
 @Composable
 fun SandClock() {
+    val alpha: Float by animateFloatAsState(1f)
+
     Canvas(modifier = Modifier.fillMaxSize()) {
         val canvasWidth = size.width
         val canvasHeight = size.height
@@ -148,7 +183,27 @@ fun SandClock() {
              color = Color.Blue,
              center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
              radius = size.minDimension / 4
+
          )*/
+
+        var running = true
+
+
+
+
+
+        withTransform(
+            {
+                rotate( 360*alpha, Offset(x = canvasWidth / 2, y = canvasHeight / 2))
+            }, {
+                drawLine(
+                    strokeWidth = 8.dp.toPx(),
+                    cap = StrokeCap.Round,
+                    color = Color.Red,
+                    start = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
+                    end = Offset(size.minDimension / 2, 12.dp.toPx()))
+            }
+        )
 
         drawLine(
             start = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
@@ -173,6 +228,8 @@ fun SandClock() {
 }
 
 
+
+@ExperimentalAnimationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
@@ -181,6 +238,7 @@ fun LightPreview() {
     }
 }
 
+@ExperimentalAnimationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
